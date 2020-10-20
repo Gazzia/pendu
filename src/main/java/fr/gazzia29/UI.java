754601d4dc0b3a36f32b172dd51970132461e138
Joseph Allain <36985411+Gazzia29@users.noreply.github.com>
2020-10-20 15:39:24 +0200

    Added base ui methods

--- src/main/java/fr/gazzia29/UI.java
+++ src/main/java/fr/gazzia29/UI.java
@@ -1,5 +1,22 @@
 package fr.gazzia29;
 
-public class UI {
+import java.util.Scanner;
 
+public class UI {
+	Scanner scanner;
+	String separator;
+
+	public final void clearConsole() {
+		for (int i = 0; i < 50; ++i)
+			System.out.println();
+	}
+
+	public static void print(Object o) {
+		System.out.println(o);
+	}
+
+	public UI() {
+		scanner = new Scanner(System.in);
+		separator = "___________________________________";
+	}
 }
